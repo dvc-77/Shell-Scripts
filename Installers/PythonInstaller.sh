1e407f9b62613692a7819017f26ee0f53ac1876e
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2023-01-09 23:57:11 +0000

    add: PythonInstaller.sh

---
+++ Installers/PythonInstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Python
+sudo apt-get update
+sudo apt-get install python3
+
+# Install recommended extensions
+code --install-extension ms-python.python
+code --install-extension donjayamanne.python-extension-pack
+code --install-extension ms-vscode.vscode-python-docstring
